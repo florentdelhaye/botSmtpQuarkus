c9367997716ac07f7332a75b3952668484cb15c7
Florent Delhaye <florent.delhaye@gmail.com>
2023-01-25 18:22:18 +0100

    Add files

---
+++ src/main/java/TrackingServer.java
@@ -0,0 +1,29 @@
+import org.jboss.resteasy.reactive.ResponseStatus;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/tracking")
+public class TrackingServer {
+
+    @GET
+    @Path("/o")
+    @ResponseStatus(200)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String open() {
+        System.out.println("o");
+        return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path("/c")
+    @ResponseStatus(200)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String click() {
+        System.out.println("c");
+        return "Hello from RESTEasy Reactive";
+    }
+
+}
